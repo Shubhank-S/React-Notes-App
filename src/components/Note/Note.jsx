487eab3ade1c87a787f2376a4d17f8d2888e4b5b
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-12-15 20:40:56 +0530

    Delete Functionality added

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,13 +1,23 @@
 import { MdDeleteForever } from "react-icons/md";
+import useNoteContext from "../../hooks/useNoteContext";
 import "./Note.css";
 
 function Note({ id, text, date }) {
+  const { notes, setNotes, addNote } = useNoteContext();
+
+  const deleteNote = (id) => {
+    const deleteNotes = notes.filter((note) => note.id !== id);
+    setNotes(deleteNotes);
+  };
   return (
     <div className="note">
       <span>{text}</span>
       <div className="note_footer">
         <small>{date}</small>
-        <MdDeleteForever className="note_delete_icon" />
+        <MdDeleteForever
+          className="note_delete_icon"
+          onClick={() => deleteNote(id)}
+        />
       </div>
     </div>
   );
